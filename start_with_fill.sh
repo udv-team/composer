da38c330232d22ff44acb8942ef437c680be31d8
Булыгин Евгений Андреевич <ebulygin@ussc.ru>
2022-01-06 19:24:08 +0500

    Изменил email

--- start_with_fill.sh
+++ start_with_fill.sh
@@ -8,6 +8,6 @@ BLUE='\033[0;34m'
 echo -e ${RED}"Останавливаем и удаляем запущенные контейнеры" && docker-compose down -v &&
   echo -e ${BLUE}"Загружаем образ" && docker-compose pull &&
   echo -e ${BLUE}"Запускаем контейнеры" && docker-compose up -d &&
-  echo -e ${BLUE}"Ожидаем поднятия сервисов" && sleep 10 &&
+  echo -e ${BLUE}"Ожидаем поднятия сервисов" && sleep 30 &&
   echo -e ${BLUE}"Запускаем заливку" && docker exec -it cas python3 -m src.fill_data &&
   echo -e ${GREEN}Запуск проекта успешно выполнен
