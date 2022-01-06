cbe5e5fb3e9e2f61fea9b977aa9a786ecfd0c59d
Булыгин Евгений Андреевич <ebulygin@ussc.ru>
2022-01-06 18:17:11 +0500

    Изменил email

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-for address in 85.208.208.13 45.144.64.103
+for address in 45.144.64.103
 do
   echo 'Обновляем сервер' $address
   scp docker-compose.yaml start_with_fill.sh root@$address:/root/udv-team
